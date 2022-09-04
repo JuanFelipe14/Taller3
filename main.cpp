95e0abc0912707e2ca20ba4f8d7f036284d292bc
Pipe-A <70495622+Pipe-A@users.noreply.github.com>
2022-09-04 16:12:54 -0500

    Completo sin todos los atributos

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <fstream>
 #include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -12,45 +13,39 @@ struct Nodo{
     Nodo *der;
 };
 
-void menu();
+void menu(vector<Nodo> personas);
 Nodo *crearNodo(int edad, string nombre);
 void insertarNodo(Nodo *&arbol, int n, string nombre);
 void mostrarArbol(Nodo *arbol, int contador);
 bool buscarNodo(Nodo *arbol, int n);
+void mostrarMenores(Nodo *arbol, int contador);
+void mostrarMayores(Nodo *arbol, int contador);
 
 Nodo *arbol = NULL;
 
-fstream MyFile;
-
 int main() {
 
-    fstream ReadFile("C://Users//juanf//OneDrive//Desktop//Taller 3 archivos adjuntos 4 de septiembre de 2022 0948//data.csv",ios::in);
+    vector <Nodo> personas;
+    Nodo persona;
+    string nombreArchivo = "C://Users//juanf//OneDrive//Desktop//Taller3//data.csv";
+    ifstream archivo(nombreArchivo.c_str());
+    string linea;
+    string nombre, apellido, sexo, estadoCivil,edad;
 
-    if(!ReadFile){
-        cout << "Archivo inexistente" << endl;
-    }
+  	while (getline(archivo, linea))
+	{
+		stringstream input_stringstream(linea);
+		getline(input_stringstream, nombre, ',');
+        getline(input_stringstream, apellido, ',');
+		getline(input_stringstream, sexo, ',');
+		getline(input_stringstream, estadoCivil, ',');
+		getline(input_stringstream, edad, ',');
+        persona.edad = stoi(edad); 
+        persona.nombre = nombre; 
+        personas.push_back(persona);  
+	}
 
-    MyFile.open("C://Users//juanf//OneDrive//Desktop//Taller 3 archivos adjuntos 4 de septiembre de 2022 0948//data.csv", ios::app);
-
-    string line;
-    vector<string> datos;
-    vector<string> datos_lineas;
-
-    ifstream lector("C://Users//juanf//OneDrive//Desktop//Taller 3 archivos adjuntos 4 de septiembre de 2022 0948//data.csv");
-
-    if(lector.is_open()){
-        while(getline(lector,line)){
-            datos.push_back(line);
-        }
-
-        for(int i = 0 ; i < datos.size();i++){
-            cout << datos[i];
-        }
-    }
-
-    MyFile.close();
-
-    menu();
+    menu(personas);
 
     return 0;
 }
@@ -89,7 +84,7 @@ void mostrarArbol(Nodo *arbol, int contador){
         mostrarArbol(arbol->der, contador+1);
 
         for(int i = 0 ; i < contador; i++){
-            cout << "           "; //Cambiar
+            cout << "           ";
         }
 
         cout << arbol->nombre << endl;
@@ -110,27 +105,59 @@ bool buscarNodo(Nodo *arbol, int n){
     }
 }
 
-void menu(){
+void mostrarMenores(Nodo *arbol, int contador){
+    if(arbol == NULL){
+        return;
+    }else{
+        mostrarArbol(arbol->izq, contador+1);
+
+        for(int i = 0 ; i < contador; i++){
+            cout << "           ";
+        }
+
+        cout << arbol->nombre << endl;
+    }
+}
+
+void mostrarMayores(Nodo *arbol, int contador){
+    if(arbol == NULL){
+        return;
+    }else{
+        mostrarArbol(arbol->der, contador+1);
+
+        for(int i = 0 ; i < contador; i++){
+            cout << "           ";
+        }
+
+        cout << arbol->nombre << endl;
+    }
+}
+
+
+void menu(vector<Nodo> personas){
 
     int dato, opcion, cont = 0;
     string nombre;
 
     do{
         cout << " MENU " << endl;
-        cout << " 1. Insertar un nuevo nodo" << endl;
+        cout << " 1. Llenar arbol" << endl;
         cout << " 2. Mostrar el arbol completo" << endl;
         cout << " 3. Buscar edad en el arbol" << endl;
-        cout << " 4. Salir" << endl;
+        cout << " 4. Mostrar familia menor que la tia" << endl;
+        cout << " 5. Mostrar familia mayor que la tia" << endl;
+        cout << " 6. Salir" << endl;
         cout << "-> ";
         cin >> opcion;
 
         switch (opcion) {
             case 1:
-                cout << "Digite la edad: ";
-                cin >> dato;
-                cout << "\nDigite el nombre del familiar :  ";
-                cin >> nombre;
-                insertarNodo(arbol, dato,nombre);
+
+                for(int i = 0 ; i < personas.size();i++){
+                    insertarNodo(arbol, personas[i].edad,personas[i].nombre);
+                }
+
+                cout << "Nodos llenados con exito" << endl;
                 system("pause");
                 break;
 
@@ -148,10 +175,16 @@ void menu(){
                         cout << "La edad NO existe" << endl;
                     }
                     cout << "\n";
-                    system("pause");
                     break;
+
+            case 4:
+                mostrarMenores(arbol,cont);
+                break;
+            case 5:
+                mostrarMayores(arbol, cont);
+                break;
         }
 
         system("cls");
-    }while(opcion != 4);
+    }while(opcion != 6);
 }
